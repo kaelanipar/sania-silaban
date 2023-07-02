37d2f3e8ec24f545c346dd3179c2c91ebe7a2d3d
kaelanipar <michaelsianipar86399@gmail.com>
2023-07-03 02:38:43 +0700

    commit nia

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika bar menu di klik
+document.querySelector("#bar-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Toggle class active untuk search form
+
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// klik di luar slide bar untuk menghilangkan nav
+const hm = document.querySelector("#bar-menu");
+const sb = document.querySelector("#search-button");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+});
